df5590b64a6c4dc0d7e1a14ffd0d9250895f1ccb
rtheni <rntm72@gmail.com>
2020-08-26 22:10:10 -0300

    Updates

---
+++ precache-manifest.8a206327beac325cd477c143ab885b29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfee1914a2c3793fbb1b3479d6c41ac",
+    "url": "/ProyectoModulo1Matea-MatiasRivas-RafaelTheni/index.html"
+  },
+  {
+    "revision": "98ebace60ac548256c1c",
+    "url": "/ProyectoModulo1Matea-MatiasRivas-RafaelTheni/static/css/main.e67fe502.chunk.css"
+  },
+  {
+    "revision": "85af8aa57b42f6f92ba0",
+    "url": "/ProyectoModulo1Matea-MatiasRivas-RafaelTheni/static/js/2.dba7d7a7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ProyectoModulo1Matea-MatiasRivas-RafaelTheni/static/js/2.dba7d7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ebace60ac548256c1c",
+    "url": "/ProyectoModulo1Matea-MatiasRivas-RafaelTheni/static/js/main.fff3b096.chunk.js"
+  },
+  {
+    "revision": "abd1f39df8623caca1b5",
+    "url": "/ProyectoModulo1Matea-MatiasRivas-RafaelTheni/static/js/runtime-main.007e9fd5.js"
+  }
+]);
